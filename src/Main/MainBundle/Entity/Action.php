351247544dcb472805d50726a8fdb1c1246dd185
l0ic68 <maxiloic@gmail.com>
2017-04-26 09:53:02 +0200

    Ajout champs entity
    Action
    Media
    News

--- src/Main/MainBundle/Entity/Action.php
+++ src/Main/MainBundle/Entity/Action.php
@@ -21,6 +21,54 @@ class Action
      */
     private $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_debut", type="datetime")
+     */
+    private $date_debut;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_fin", type="datetime")
+     */
+    private $date_fin;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="cout", type="integer")
+     */
+    private $cout;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="recolte", type="integer")
+     */
+    private $recolte;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
 
     /**
      * Get id
@@ -30,5 +78,166 @@ class Action
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return Action
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Action
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set date_debut
+     *
+     * @param \DateTime $dateDebut
+     * @return Action
+     */
+    public function setDateDebut($dateDebut)
+    {
+        $this->date_debut = $dateDebut;
+
+        return $this;
+    }
+
+    /**
+     * Get date_debut
+     *
+     * @return \DateTime 
+     */
+    public function getDateDebut()
+    {
+        return $this->date_debut;
+    }
+
+    /**
+     * Set date_fin
+     *
+     * @param \DateTime $dateFin
+     * @return Action
+     */
+    public function setDateFin($dateFin)
+    {
+        $this->date_fin = $dateFin;
+
+        return $this;
+    }
+
+    /**
+     * Get date_fin
+     *
+     * @return \DateTime 
+     */
+    public function getDateFin()
+    {
+        return $this->date_fin;
+    }
+
+    /**
+     * Set cout
+     *
+     * @param integer $cout
+     * @return Action
+     */
+    public function setCout($cout)
+    {
+        $this->cout = $cout;
+
+        return $this;
+    }
+
+    /**
+     * Get cout
+     *
+     * @return integer 
+     */
+    public function getCout()
+    {
+        return $this->cout;
+    }
+
+    /**
+     * Set recolte
+     *
+     * @param integer $recolte
+     * @return Action
+     */
+    public function setRecolte($recolte)
+    {
+        $this->recolte = $recolte;
+
+        return $this;
+    }
+
+    /**
+     * Get recolte
+     *
+     * @return integer 
+     */
+    public function getRecolte()
+    {
+        return $this->recolte;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Action
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
     }
 }
