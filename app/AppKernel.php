1c91860a8ef318d17981989557dab95ce1fe530b
l0ic68 <maxiloic@gmail.com>
2017-04-27 09:01:59 +0200

    Delete AppBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Main\MainBundle\MainBundle(),
             new User\UserBundle\UserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
